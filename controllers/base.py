2be02405501e9b01c60c25951f710d87781f3947
Rob Prestemon <rob.prestemon@gmail.com>
2017-07-04 20:20:41 -0700

    cleanup solution and readme

--- controllers/base.py
+++ controllers/base.py
@@ -18,36 +18,36 @@ class BaseController(CementBaseController):
             users = session.query(User).all()
             num_users = len(users)
 
-            # Set default group size to lowest possible
-            group_size = 3
-
-            # If number is divisible by 5, or remainder is greater than 3, use 5 as base size
-            if num_users % 5 == 0 or num_users % 5 >= 3:
-                group_size = 5
-            # Repeat for 4
-            elif num_users % 4 == 0 or num_users % 4 >= 3:
-                group_size = 4
-
-            self.app.log.info(
-                'Creating groups with base size {size}, last group size {extra}'
-                              .format(size=group_size, extra=num_users % group_size))
-
             # Randomly shuffle users before grouping
             shuffle(users)
-            groups = list(self.set_groups(users, group_size))
+
+            # set up empty list for groups
+            groups = []
+
+            # set initial start index for slicing the user list
+            start_index = 0
+
+            while num_users > 0:
+                # If number is divisible by 5, or remainder is greater than 3, use 5 as base size
+                # The >= 5 is necessary since num % 5 returns the numerator as the result
+                if num_users >= 5 and (num_users % 5 == 0 or num_users % 5 >= 3):
+                    group_size = 5
+                # Repeat for 4
+                elif num_users >= 4 and (num_users % 4 == 0 or num_users % 4 >= 3):
+                    group_size = 4
+                else:
+                    group_size = 3
+
+                user_group = users[start_index:group_size + start_index]
+                groups.append(user_group)
+
+                # increase the start index by the current group size
+                start_index += group_size
+                # decrease the number of users by the current group size to allow loop to end
+                num_users -= group_size
 
             for group in groups:
-                self.app.log.info(group)
-                print('\n')
+                print(group, '\n')
 
         except Exception as e:
             self.app.log.error(e)
-
-    def set_groups(self, users, group_size):
-        """
-        Generator that returns groups of group_size from users
-        Found this syntax here https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
-        and modified naming for clarity in our use case
-        """
-        for i in range(0, len(users), group_size):
-            yield users[i:i + group_size]
